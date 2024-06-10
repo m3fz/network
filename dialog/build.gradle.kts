45cfce1098d465cbe9833cc6269e1388b39002f1
Mikhail Nugumanov <mnugumanov@rbc.ru>
2024-06-10 18:25:16 +0300

    Added business metrics

--- dialog/build.gradle.kts
+++ dialog/build.gradle.kts
@@ -8,4 +8,6 @@ version = "0.0.1"
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-redis")
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.3.0")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    runtimeOnly("io.micrometer:micrometer-registry-prometheus")
 }
